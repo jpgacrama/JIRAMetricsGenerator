1dd142f2a68106d5306715e3f366c3ed9cf0ff5a
Jonas Gacrama <jonasgacrama@gmail.com>
2021-05-16 14:20:44 +0800

    Renamed the Class which handles JIRA queries to JIRAService
    I prefer composition over inheritance. I just need to fix the composition

--- jiraMetricsGenerator.py
+++ jiraMetricsGenerator.py
@@ -13,9 +13,10 @@ members = {
     'Austin': '5fbb3d037cc1030069500950'
 }
 
-class JIRALoginAndQuery():
+class JIRAService(object):
     username = None
     api_token = None
+    jiraService = None
     
     def __init__(self):
         pass
@@ -23,62 +24,62 @@ class JIRALoginAndQuery():
     def logInToJIRA(self):
         username = input("Please enter your username: ")
         api_token = input("Please enter your api-token: ")
-        jiraLogInResult = JIRA(URL, basic_auth=(username, api_token))
-        return jiraLogInResult
+        self.jiraService = JIRA(URL, basic_auth=(username, api_token))
 
     def queryJIRA(self):
         # NOTE:
         # You need to create two JIRA queries: one where you can access the worklog
         # And another to access the Issue types... WHY? 
-        allIssuesFromAustin = self.jira.search_issues(
+        allIssuesFromAustin = self.jiraService.search_issues(
             f'assignee in ({members["Austin"]}) AND project = {PROJECT}')
 
-        allWorklogsFromAustin = self.jira.search_issues(
+        allWorklogsFromAustin = self.jiraService.search_issues(
             f'assignee in ({members["Austin"]}) AND project = {PROJECT}', fields="worklog")
 
         return allIssuesFromAustin, allWorklogsFromAustin
 
 class GenerateMetrics(object):
-    jira = None
+    allIssues = None
+    allWorklogs = None
     
-    def __init__(self, jira):
-        self.jira = jira
+    def __init__(self, allIssues, allWorklogs):
+        self.allIssues = allIssues
+        self.allWorklogs = allWorklogs
 
     def getDesiredMonth(self):
         month = input("Enter the desired Month in number form: ")
         return int(month)
 
-    def getAllInProgressTasksFromAustin(self, jira):
+    def getAllInProgressWorklogs(self):
         desiredMonth = self.getDesiredMonth()
 
-
-        
-        for value in allIssuesFromAustin:
-            print(dir(value.fields.issuetype))
+        for value in self.allWorklogs:
+            # print(dir(value.fields.issuetype))
             
-            # log_entry_count = len(value.fields.worklog.worklogs)
+            log_entry_count = len(value.fields.worklog.worklogs)
 
-            # for i in range(log_entry_count):
-            #     dateOfLog = value.fields.worklog.worklogs[i].updated
-            #     dateOfLog = dateOfLog.split(" ")
-            #     dateOfLog[-1] = dateOfLog[-1][:10]
-            #     dateOfLog = " ".join(dateOfLog)
-            #     extractedDateTime = datetime.strptime(dateOfLog, "%Y-%m-%d")
+            for i in range(log_entry_count):
+                dateOfLog = value.fields.worklog.worklogs[i].updated
+                dateOfLog = dateOfLog.split(" ")
+                dateOfLog[-1] = dateOfLog[-1][:10]
+                dateOfLog = " ".join(dateOfLog)
+                extractedDateTime = datetime.strptime(dateOfLog, "%Y-%m-%d")
 
-            #     if extractedDateTime.month == desiredMonth:
-            #         print(dir(value.fields))
-            #         print ( value.key, 
-            #                 value.fields.issuetype.description,
-            #                 value.fields.worklog.worklogs[i].timeSpent,
-            #                 value.fields.worklog.worklogs[i].updateAuthor,
-            #                 value.fields.worklog.worklogs[i].updated)
+                if extractedDateTime.month == desiredMonth:
+                    print(dir(value.fields))
+                    print ( value.key, 
+                            value.fields.issuetype.description,
+                            value.fields.worklog.worklogs[i].timeSpent,
+                            value.fields.worklog.worklogs[i].updateAuthor,
+                            value.fields.worklog.worklogs[i].updated)
 
 def main():
-    jiraQuery = JIRALoginAndQuery()
-    jiraLogInResult = jiraQuery.logInToJIRA()
-    
+    jiraService = JIRAService()
+    jiraService.logInToJIRA()
+    allIssuesFromAustin, allWorklogsFromAustin = jiraService.queryJIRA()
+
     metrics = GenerateMetrics(jiraLogInResult)
-    metrics.getAllInProgressTasksFromAustin(jiraLogInResult)
+    metrics.getAllInProgressWorklogs(jiraLogInResult)
 
 if __name__ == "__main__":
     main()
