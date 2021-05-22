50eec44ced5e1b60caf5d9d59cc462265fa9a573
Jonas Gacrama <jonasgacrama@gmail.com>
2021-05-22 15:43:48 +0800

    I need to find out why the automated sum does not match my manual data

--- jiraMetricsGenerator.py
+++ jiraMetricsGenerator.py
@@ -9,20 +9,20 @@ import pandas as pd
 URL = 'https://macrovue.atlassian.net'
 PROJECT = 'OMNI'
 MEMBERS = {
-    'Arman'     : '6057df8914a23b0069a65dc8',
-    'Austin'    : '5fbb3d037cc1030069500950',
-    'Duane'     : '5efbf73454020e0ba82ac7a0',
-    'Eddzonne'  : '5f85328a53aaa400760d4944',
-    'Florante'  : '5fa0b7ad22f39900769a8242',
-    'Jansseen'  : '5f3b1fd49aa9650046aeffb6',
+    # 'Arman'     : '6057df8914a23b0069a65dc8',
+    # 'Austin'    : '5fbb3d037cc1030069500950',
+    # 'Duane'     : '5efbf73454020e0ba82ac7a0',
+    # 'Eddzonne'  : '5f85328a53aaa400760d4944',
+    # 'Florante'  : '5fa0b7ad22f39900769a8242',
+    # 'Jansseen'  : '5f3b1fd49aa9650046aeffb6',
     'Jaypea'    : '6073ef399361560068ad4b83',
-    'Jerred'    : '5ed4c8d844cc830c23027b31',
-    'Juliet'    : '5fa89a11ecdae600684d1dc8',
-    'Marwin'    : '600e2429cd564b0068e7cca7',
-    'Mary'      : '6099e1699b362f006957e1ad',
-    'Maye'      : '6099d80c3fae6f006821f3f5',
-    'Nicko'     : '5f3b1fd4ea5e2f0039697b3d',
-    'Ranniel'   : '604fe79ce394c300699ce0ed',
+    # 'Jerred'    : '5ed4c8d844cc830c23027b31',
+    # 'Juliet'    : '5fa89a11ecdae600684d1dc8',
+    # 'Marwin'    : '600e2429cd564b0068e7cca7',
+    # 'Mary'      : '6099e1699b362f006957e1ad',
+    # 'Maye'      : '6099d80c3fae6f006821f3f5',
+    # 'Nicko'     : '5f3b1fd4ea5e2f0039697b3d',
+    # 'Ranniel'   : '604fe79ce394c300699ce0ed',
     'Ronald'    : '5fb1f35baa1d30006fa6a618'
 }
 
@@ -69,11 +69,14 @@ def plotData(dictionaryWorklog, person):
 def computeTotalTimeSpent(numberOfJiraTicketsForEachSW, jiraValue, dictionaryWorklog, sw, month):
     timeHelper = TimeHelper()
     previousKey = None
+    totalTimeSpent = 0
+    newTimeSpent = 0
 
     for i in range(numberOfJiraTicketsForEachSW):
         extractedDateTime = timeHelper.trimDate(jiraValue, i)
         if extractedDateTime.month == month:
             if previousKey != jiraValue.key:
+                totalTimeSpent = 0                
                 previousKey = jiraValue.key
                 totalTimeSpent = jiraValue.fields.worklog.worklogs[i].timeSpentSeconds
                 totalTimeSpent = timeHelper.convertToHours(totalTimeSpent)
@@ -83,7 +86,7 @@ def computeTotalTimeSpent(numberOfJiraTicketsForEachSW, jiraValue, dictionaryWor
                 newTimeSpent = jiraValue.fields.worklog.worklogs[i].timeSpentSeconds
                 newTimeSpent = timeHelper.convertToHours(newTimeSpent)
                 totalTimeSpent += newTimeSpent
-                dictionaryWorklog[sw][jiraValue.key] = totalTimeSpent
+                dictionaryWorklog[sw][previousKey] = totalTimeSpent
 
     return dictionaryWorklog
 
