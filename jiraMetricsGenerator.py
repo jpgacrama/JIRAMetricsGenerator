ad46dc0bd300e58dadfc06d59f5726c6bfdb2592
Jonas Gacrama <gacrama.jonas@gmail.com>
2021-08-14 17:35:12 +0800

    Now correct for both Arman and Austin
    Now I need to add a third person

--- jiraMetricsGenerator.py
+++ jiraMetricsGenerator.py
@@ -14,21 +14,21 @@ PROJECT = 'OMNI'
 MEMBERS = {
     'Arman'     : '6057df8914a23b0069a65dc8',
     'Austin'    : '5fbb3d037cc1030069500950',
-    'Daniel'    : '61076053fc68c10069c80eba',
-    'Duane'     : '5efbf73454020e0ba82ac7a0',
-    'Eddzonne'  : '5f85328a53aaa400760d4944',
-    'Florante'  : '5fa0b7ad22f39900769a8242',
-    'Harold'    : '60aaff8d5dc18500701239c0',
-    'Jansseen'  : '5f3b1fd49aa9650046aeffb6',
-    'Jaypea'    : '6073ef399361560068ad4b83',
-    'Jerred'    : '5ed4c8d844cc830c23027b31',
-    'Juliet'    : '5fa89a11ecdae600684d1dc8',
-    'Marwin'    : '600e2429cd564b0068e7cca7',
-    'Mary'      : '6099e1699b362f006957e1ad',
-    'Maye'      : '6099d80c3fae6f006821f3f5',
-    'Nicko'     : '5f3b1fd4ea5e2f0039697b3d',
-    'Ranniel'   : '604fe79ce394c300699ce0ed',
-    'Ronald'    : '5fb1f35baa1d30006fa6a618'
+    # 'Daniel'    : '61076053fc68c10069c80eba'
+    # 'Duane'     : '5efbf73454020e0ba82ac7a0',
+    # 'Eddzonne'  : '5f85328a53aaa400760d4944',
+    # 'Florante'  : '5fa0b7ad22f39900769a8242',
+    # 'Harold'    : '60aaff8d5dc18500701239c0',
+    # 'Jansseen'  : '5f3b1fd49aa9650046aeffb6',
+    # 'Jaypea'    : '6073ef399361560068ad4b83',
+    # 'Jerred'    : '5ed4c8d844cc830c23027b31',
+    # 'Juliet'    : '5fa89a11ecdae600684d1dc8',
+    # 'Marwin'    : '600e2429cd564b0068e7cca7',
+    # 'Mary'      : '6099e1699b362f006957e1ad',
+    # 'Maye'      : '6099d80c3fae6f006821f3f5',
+    # 'Nicko'     : '5f3b1fd4ea5e2f0039697b3d',
+    # 'Ranniel'   : '604fe79ce394c300699ce0ed',
+    # 'Ronald'    : '5fb1f35baa1d30006fa6a618'
 }
 
 ISSUE_TYPES = ['Project', 'Ad-hoc']
@@ -314,8 +314,10 @@ class ThreadMatrixOfWorklogsPerSW(threading.Thread):
         self.timeSpentPerSoftware = timeSpentPerSoftware
 
     def run(self):
+        print(f'Getting data for: {self.person}')
         self.timeSpentPerSoftware.extractItemsPerSW(self.person, self.jiraService)
         self.worklog[self.person] = self.timeSpentPerSoftware.getTimeSpentForEachSW()
+        print(self.worklog)
 
 # This will be the "Caller" class
 class MatrixOfWorklogsPerSW(object):
@@ -342,6 +344,9 @@ class MatrixOfWorklogsPerSW(object):
 
         print("\n-------- GENERATING MATRIX OF TIME SPENT PER SW --------\n")
 
+        for person in MEMBERS:
+            self.worklog[person] = {}
+
         threads = [ThreadMatrixOfWorklogsPerSW(
                 person, self.jiraService, self.worklog, timeSpentPerSoftware) for person in MEMBERS]
 
@@ -352,9 +357,9 @@ class MatrixOfWorklogsPerSW(object):
         for thread in threads:
             thread.join()
 
-        for person in MEMBERS:
-            for thread in threads:
-                self.worklog[person] = thread.worklog
+        # for person in MEMBERS:
+        #     for thread in threads:
+        #         self.worklog[person] = thread.worklog
 
         # for person in MEMBERS:
         #     timeSpentPerSoftware.extractItemsPerSW(person, self.jiraService)
