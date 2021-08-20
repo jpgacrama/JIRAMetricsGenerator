3e821b30ab713772911218cc1a23f8b6403dcf5d
Jonas Gacrama <gacrama.jonas@gmail.com>
2021-08-20 22:16:26 +0800

    Uncommenting the rest

--- jiraMetricsGenerator.py
+++ jiraMetricsGenerator.py
@@ -789,25 +789,25 @@ def main():
     os.system('cls' if os.name == 'nt' else 'clear')
     jiraService = JIRAService()
 
-    # matrixOfWorklogsPerSW = HoursSpentPerSW(jiraService)
-    # matrixOfWorklogsPerSW.extractTimeSpentPerSW()
-    # matrixOfWorklogsPerSW.writeToCSVFile()
+    matrixOfWorklogsPerSW = HoursSpentPerSW(jiraService)
+    matrixOfWorklogsPerSW.extractTimeSpentPerSW()
+    matrixOfWorklogsPerSW.writeToCSVFile()
 
-    # timeSpentPerPerson = TimeSpentPerPerson(jiraService)
-    # timeSpentPerPerson.extractTimeSpentPerPerson()
-    # timeSpentPerPerson.generateCSVFile()
+    timeSpentPerPerson = TimeSpentPerPerson(jiraService)
+    timeSpentPerPerson.extractTimeSpentPerPerson()
+    timeSpentPerPerson.generateCSVFile()
 
-    # doneItemsPerPerson = DoneItemsPerPerson(jiraService)
-    # doneItemsPerPerson.extractDoneItemsPerPerson()
-    # doneItemsPerPerson.generateCSVFile()
+    doneItemsPerPerson = DoneItemsPerPerson(jiraService)
+    doneItemsPerPerson.extractDoneItemsPerPerson()
+    doneItemsPerPerson.generateCSVFile()
 
     unfinishedItemsPerPerson = UnfinishedItemsPerPerson(jiraService)
     unfinishedItemsPerPerson.extractUnfinishedItemsPerPerson()
     unfinishedItemsPerPerson.generateCSVFile()
 
-    # rawItemsPerPerson = RawItemsPerPerson(jiraService)
-    # rawItemsPerPerson.extractRawItemsPerPerson()
-    # rawItemsPerPerson.generateCSVFile()
+    rawItemsPerPerson = RawItemsPerPerson(jiraService)
+    rawItemsPerPerson.extractRawItemsPerPerson()
+    rawItemsPerPerson.generateCSVFile()
 
 if __name__ == "__main__":
     main()
