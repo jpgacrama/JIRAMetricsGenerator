50d3fcb8d30615fee75365cf42619bd4df1ff4b2
Jonas Gacrama <jonas.gacrama@flexisourceit.com.au>
2021-12-06 14:44:46 +0800

    Double-check if the bug does not happen when I am using JIRA by myself

--- jiraMetricsGenerator.py
+++ jiraMetricsGenerator.py
@@ -15,41 +15,44 @@ PROJECT = 'OMNI'
 
 MEMBERS = {
     'Arman'     : '6057df8914a23b0069a65dc8',
-    'Austin'    : '5fbb3d037cc1030069500950',
-    'Correne'   : '616cc99920972200718e6d86',
-    'Daniel'    : '61076053fc68c10069c80eba',
-    'Duane'     : '5efbf73454020e0ba82ac7a0',
-    'Eddzonne'  : '5f85328a53aaa400760d4944',
-    'Florante'  : '5fa0b7ad22f39900769a8242',
-    'Jansseen'  : '5f3b1fd49aa9650046aeffb6',
-    'Jaypea'    : '6073ef399361560068ad4b83',
-    'Jerred'    : '5ed4c8d844cc830c23027b31',
-    'Joppet'    : '618a332c137a51006a46ea0a',
-    'Juliet'    : '5fa89a11ecdae600684d1dc8',
-    'Marwin'    : '600e2429cd564b0068e7cca7',
-    'Mary'      : '6099e1699b362f006957e1ad',
-    'Maye'      : '6099d80c3fae6f006821f3f5',
-    'Nicko'     : '5f3b1fd4ea5e2f0039697b3d',
-    'Ronald'    : '5fb1f35baa1d30006fa6a618',
+    # 'Austin'    : '5fbb3d037cc1030069500950',
+    # 'Correne'   : '616cc99920972200718e6d86',
+    # 'Daniel'    : '61076053fc68c10069c80eba',
+    # 'Duane'     : '5efbf73454020e0ba82ac7a0',
+    # 'Eddzonne'  : '5f85328a53aaa400760d4944',
+    # 'Florante'  : '5fa0b7ad22f39900769a8242',
+    # 'Jansseen'  : '5f3b1fd49aa9650046aeffb6',
+    # 'Jaypea'    : '6073ef399361560068ad4b83',
+    # 'Jerred'    : '5ed4c8d844cc830c23027b31',
+    # 'Joppet'    : '618a332c137a51006a46ea0a',
+    # 'Juliet'    : '5fa89a11ecdae600684d1dc8',
+    # 'Marwin'    : '600e2429cd564b0068e7cca7',
+    # 'Mary'      : '6099e1699b362f006957e1ad',
+    # 'Maye'      : '6099d80c3fae6f006821f3f5',
+    # 'Nicko'     : '5f3b1fd4ea5e2f0039697b3d',
+    # 'Ronald'    : '5fb1f35baa1d30006fa6a618',
 }
 
 ISSUE_TYPES = ['Project', 'Ad-hoc']
 
 SOFTWARE = [
-    'Infrastructure',
     'AAIG CRM',
+    'Ascot',
+    'ASR Marketing',
     'ASR Reports',
-    'Wordpress CMS Websites',
+    'ASRW Marketing',
+    'CMA',
+    'HALO',
+    'HALO Marketing',
+    'HALO Mobile',
     'Hubspot CMS Websites',
+    'Infrastructure',
     'Macrovue',
     'Macrovue Marketing',
-    'HALO',
-    'HALO Mobile',
-    'HALO Marketing',
     'Notification',
-    'Ascot',
-    'CMA',
-    'R:Ed']
+    'R:Ed',
+    'Wordpress CMS Websites'
+    ]
 
 # Filenames for the output files
 TIME_SPENT_PER_SW = 'HoursDistributionPerSW.csv'
@@ -808,11 +811,11 @@ def main():
     try:
         loop = asyncio.get_event_loop()
         tasks = [
-            loop.create_task(matrixOfWorklogsPerSW.extractTimeSpentPerSW()),
-            loop.create_task(timeSpentPerPerson.extractTimeSpentPerPerson()),
+            # loop.create_task(matrixOfWorklogsPerSW.extractTimeSpentPerSW()),
+            # loop.create_task(timeSpentPerPerson.extractTimeSpentPerPerson()),
             loop.create_task(doneItemsPerPerson.extractDoneItemsPerPerson()),
-            loop.create_task(unfinishedItemsPerPerson.extractUnfinishedItemsPerPerson()),
-            loop.create_task(rawItemsPerPerson.extractRawItemsPerPerson()),
+            # loop.create_task(unfinishedItemsPerPerson.extractUnfinishedItemsPerPerson()),
+            # loop.create_task(rawItemsPerPerson.extractRawItemsPerPerson()),
         ]
         start = time.perf_counter()
         loop.run_until_complete(asyncio.wait(tasks))
