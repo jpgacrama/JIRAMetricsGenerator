d4d74e5acdd8cb69000d2e7c8dae80b2618f4364
Jonas Gacrama <jonas.gacrama@flexisourceit.com.au>
2022-03-04 08:24:46 +0800

    Initial commit to automate the story point correlation

--- jiraMetricsGenerator.py
+++ jiraMetricsGenerator.py
@@ -891,11 +891,11 @@ def main():
         loop = asyncio.get_event_loop()
         tasks = [
             loop.create_task(matrixOfWorklogsPerSW.extractTimeSpentPerSW()),
-            loop.create_task(timeSpentPerPerson.extractTimeSpentPerPerson()),
-            loop.create_task(doneItemsPerPerson.extractDoneItemsPerPerson()),
-            loop.create_task(unfinishedItemsPerPerson.extractUnfinishedItemsPerPerson()),
-            loop.create_task(rawItemsPerPerson.extractRawItemsPerPerson()),
-            loop.create_task(storyPointCorrelation.computeStoryPointCorrelation()),
+            # loop.create_task(timeSpentPerPerson.extractTimeSpentPerPerson()),
+            # loop.create_task(doneItemsPerPerson.extractDoneItemsPerPerson()),
+            # loop.create_task(unfinishedItemsPerPerson.extractUnfinishedItemsPerPerson()),
+            # loop.create_task(rawItemsPerPerson.extractRawItemsPerPerson()),
+            # loop.create_task(storyPointCorrelation.computeStoryPointCorrelation()),
         ]
         start = time.perf_counter()
         loop.run_until_complete(asyncio.wait(tasks))
