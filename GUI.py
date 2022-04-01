4afdeb1864e184251bc7b59b84268dda7c10e492
Jonas Gacrama <gacrama.jonas@gmail.com>
2022-04-01 18:34:54 +0800

    GUI: End date is now extracted

--- GUI.py
+++ GUI.py
@@ -8,6 +8,8 @@ sg.theme('Default1')
 layout = [[sg.Text('Choose your date range', key='-TXT-')],
       [sg.Input(key='start_date', size=(20,1)), sg.CalendarButton(
             'Select Start Date', close_when_date_chosen=True, location=(0,0), no_titlebar=False, format='%Y-%m-%d', )],
+      [sg.Input(key='end_date', size=(20,1)), sg.CalendarButton(
+            'Select End Date', close_when_date_chosen=True, location=(0,0), no_titlebar=False, format='%Y-%m-%d', )],
       [sg.Button('Read'), sg.Exit()]]
 
 window = sg.Window('JIRA Metrics Generator', layout)
@@ -19,5 +21,8 @@ while True:
         break
     elif event == 'Read':
         startDate = values['start_date']
+        endDate = values['end_date']
+
         print(f'Start Date: {startDate}')
+        print(f'End Date: {endDate}')
 window.close()
